a65860cd67976a3661ac90a16ac81f326dc0dd1b
archey347 <archey.barrell@outlook.com>
2018-01-03 22:16:44 +0000

    Added API for retrieving blogs

--- routes/routes.php
+++ routes/routes.php
@@ -2,4 +2,6 @@
 
 $app->get('/blogs', 'UserFrosting\Sprinkle\Blog\Controller\BlogController:displayBlogAdmin')
     ->add('authGuard');
-    
+
+$app->get('/api/blogs', 'UserFrosting\Sprinkle\Blog\Controller\BlogController:getBlogs')
+    ->add('authGuard');    
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,6 +9,7 @@ use UserFrosting\Sprinkle\Core\Controller\SimpleController;
 use UserFrosting\Support\Exception\ForbiddenException;
 use UserFrosting\Sprinkle\Pastries\Database\Models\Pastry;
 use UserFrosting\Sprinkle\Core\Facades\Debug;
+use UserFrosting\Sprinkle\Blog\Sprunje\BlogSprunje;
 
 class BlogController extends SimpleController
 {
@@ -16,4 +17,31 @@ class BlogController extends SimpleController
     {
         return $this->ci->view->render($response, 'pages/blogs.html.twig');        
     }
+    
+    public function getBlogs(Request $request, Response $response, $args)
+    {
+        // GET parameters
+        $params = $request->getQueryParams();
+
+        /** @var UserFrosting\Sprinkle\Account\Authorize\AuthorizationManager */
+        $authorizer = $this->ci->authorizer;
+
+        /** @var UserFrosting\Sprinkle\Account\Database\Models\User $currentUser */
+        $currentUser = $this->ci->currentUser;
+
+        // Access-controlled page
+        if (!$authorizer->checkAccess($currentUser, 'uri_blog_manager')) {
+            throw new ForbiddenException();
+        }
+
+        /** @var UserFrosting\Sprinkle\Core\Util\ClassMapper $classMapper */
+        $classMapper = $this->ci->classMapper;
+
+        $sprunje = new BlogSprunje($classMapper, $params);
+
+        // Be careful how you consume this data - it has not been escaped and contains untrusted user-supplied content.
+        // For example, if you plan to insert it into an HTML DOM, you must escape it on the client side (or use client-side templating).
+        return $sprunje->toResponse($response);    
+    }
+    
 }
