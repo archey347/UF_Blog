0397c01b3eab3e48e6fb7fcb4ee90951e7049b5d
archey347 <archey.barrell@outlook.com>
2018-01-27 00:17:12 +0000

    Added Create Blog Function

--- routes/routes.php
+++ routes/routes.php
@@ -2,6 +2,13 @@
 
 $app->get('/blogs', 'UserFrosting\Sprinkle\Blog\Controller\BlogController:displayBlogAdmin')
     ->add('authGuard');
+	
+$app->put('/blogs', 'UserFrosting\Sprinkle\Blog\Controller\BlogController:createBlog')
+    ->add('authGuard');
+	
 
 $app->get('/api/blogs', 'UserFrosting\Sprinkle\Blog\Controller\BlogController:getBlogs')
-    ->add('authGuard');    
+    ->add('authGuard');
+	
+$app->get('/modals/blog/create', 'UserFrosting\Sprinkle\Blog\Controller\BlogController:getModalCreate')
+    ->add('authGuard');
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,12 +10,28 @@ use UserFrosting\Support\Exception\ForbiddenException;
 use UserFrosting\Sprinkle\Pastries\Database\Models\Pastry;
 use UserFrosting\Sprinkle\Core\Facades\Debug;
 use UserFrosting\Sprinkle\Blog\Sprunje\BlogSprunje;
+use UserFrosting\Sprinkle\Account\Database\Models\Permission;
+use UserFrosting\Fortress\Adapter\JqueryValidationAdapter;
+use UserFrosting\Fortress\RequestSchema;
+use UserFrosting\Fortress\ServerSideValidator;
+use UserFrosting\Fortress\RequestDataTransformer;
+use UserFrosting\Sprinkle\Blog\Database\Models\Blog;
 
 class BlogController extends SimpleController
 {
     public function displayBlogAdmin(Request $request, Response $response, $args)
     {
-        return $this->ci->view->render($response, 'pages/blogs.html.twig');        
+		
+		$schema = new RequestSchema('schema://requests/create-blog.yaml');
+		
+		$validator = new JqueryValidationAdapter($schema, $this->ci->translator);
+        return $this->ci->view->render($response, 'pages/blogs.html.twig', [
+			'page' => [
+				'validators' => [
+					'createBlog' => $validator->rules()
+				]
+			]
+		]);        
     }
     
     public function getBlogs(Request $request, Response $response, $args)
@@ -43,5 +59,79 @@ class BlogController extends SimpleController
         // For example, if you plan to insert it into an HTML DOM, you must escape it on the client side (or use client-side templating).
         return $sprunje->toResponse($response);    
     }
+    
+    public function getModalCreate(Request $request, Response $response, $args) {
+		$permissions = Permission::all();
+		$permissionOptions = array();
+		foreach($permissions as $permission) {
+			$permissionOptions[$permission['slug']] = $permission['name']. " (".$permission['slug'].")";
+		}
+        return $this->ci->view->render($response, 'modals/blog.html.twig',
+            [
+                "form" =>
+                [
+                    "submit" => "Create Blog",
+					"action" => "blogs",
+					"method" => "PUT"
+                ]
+            ]
+        );    
+    }
+	
+	public function createBlog(Request $request, Response $response, $args) {
+		// Get submitted data
+		$params = $request->getParsedBody();
+		
+		// Load the request schema
+		$schema = new RequestSchema('schema://requests/create-blog.yaml');
+		
+		// Whitelist and set parameter defaults
+		$transformer = new RequestDataTransformer($schema);
+		$data = $transformer->transform($params);
+		
+		/** @var UserFrosting\Sprinkle\Core\MessageStream $ms */
+		$ms = $this->ci->alerts;
+		
+		$validator = new ServerSideValidator($schema, $this->ci->translator);
+		
+		// Add error messages and halt if validation failed
+		if (!$validator->validate($data)) {
+			$ms->addValidationErrors($validator);
+			return $response->withStatus(400);
+		}
+		
+		// Check if that blog name already exists
+		$blogs = Blog::where('slug', $data['blog_slug'])->get();
+		if ($blogs->count()) {
+			$ms->addMessage('danger', "Blog '".$data['blog_slug']."' already exists.");
+			return $response->withStatus(400);	
+		}
+		
+		// Check that the permissions don't already exist
+		$perms = Permission::where('slug', $data['read_permission'])->get();
+		if ($blogs->count()) {
+			$ms->addMessage('danger', "Read Permission '".$data['read_permission']."' already exists.");
+			return $response->withStatus(400);	
+		}
+		
+		$perms = Permission::where('slug', $data['write_permission'])->get();
+		if ($blogs->count()) {
+			$ms->addMessage('danger', "Write Permission '".$data['write_permission']."' already exists.");
+			return $response->withStatus(400);	
+		}
+		
+		// Create Blog
+		$blog = new Blog;
+		
+		$blog->title = $data['blog_name'];
+		$blog->slug = $data['blog_slug'];
+		$blog->read_permission = $data['read_permission'];
+		$blog->write_permission = $data['write_permission'];
+		
+		$blog->save();
+		
+		$ms->addMessage('success', "Successfully added blog '".$data['blog_slug']."'.");
+		
+	}
     
 }
